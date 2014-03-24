3638622c223085638aa3b3c4da81f762c9748681
djhume <djhume@gmail.com>
2014-03-24 13:22:28 +1300

    Minor edits

--- gdx_grab.py
+++ gdx_grab.py
@@ -2,7 +2,8 @@
 gdx_grabber
 ===========
 
-Auto downloader/extractor for  vSPD GDX zip files from http://reports.ea.govt.nz/vSPDDD.aspx
+Auto downloader/extractor for  vSPD GDX zip files from
+http://reports.ea.govt.nz/vSPDDD.aspx
 
 Copyright (C) 2014 Electricity Authority, New Zealand.
 See, https://github.com/ElectricityAuthority/LICENSE/blob/master/LICENSE.md
@@ -10,22 +11,24 @@ See, https://github.com/ElectricityAuthority/LICENSE/blob/master/LICENSE.md
 Used to connect, download, extract and save daily vSPD Electricity market data.
 
 Used either on:
-  1. an adhoc basis, for occasional download and unpacking of archive files, or;
+  1. an adhoc basis, for occasional download and unpacking of archives, or;
   2. daily via crontab/scheduler of your choice:
 
 For example:
 
     python gdx_grab.py --year=2013 --archive
 
-this downloads the 2013 zip file, if not already downloaded, then extracts the individual gdx files to the extraction dir
+this downloads the 2013 zip file, if not already downloaded, then extracts
+individual gdx files to the extraction dir
 
     python gdx_grab.py --year=2008 --archive --override
-this downloads and overwrites any zip files, then extracts to extraction dir. 
+this downloads and overwrites any zip files, then extracts to extraction dir.
 
 Or, can be used daily with a crontab:
 
     15 7 * * * /usr/bin/python /home/gdxgrab/gdxgrab.py >> log.tx 2>&1
-this downloads and overwrites the last month of GDX files, as per http://reports.ea.govt.nz/vSPDDD.aspx
+this downloads and overwrites the last month of GDX files, as per
+http://reports.ea.govt.nz/vSPDDD.aspx
 
 D J Hume, 24/3/2014
 '''
